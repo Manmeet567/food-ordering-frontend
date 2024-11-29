7b32b951c6921ca5ccbabcb74ac607154b647494
Manmeet <manmeet567singh@gmail.com>
2024-11-29 12:29:27 +0530

    Add: Added Search bar to title bar of restaurant page

--- src/pages/Restaurant.jsx
+++ src/pages/Restaurant.jsx
@@ -1,24 +1,42 @@
-import React from 'react'
-import { useParams } from 'react-router-dom';
-import Navbar from '../components/Navbar/Navbar';
-import Footer from '../components/Footer/Footer';
-import RestaurantBanner from '../components/RestaurantComponents/RestaurantBanner/RestaurantBanner';
-import TitleBar from '../components/TitleBar/TitleBar';
+import {useState} from "react";
+import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer/Footer";
+import RestaurantBanner from "../components/RestaurantComponents/RestaurantBanner/RestaurantBanner";
+import TitleBar from "../components/TitleBar/TitleBar";
 import { useLocation } from "react-router-dom";
+import search from "../assets/Search More.png";
 
+const SearchBar = () => {
+  const [query, setQuery] = useState("");
+
+  return (
+    <div className="tb-sm-sb">
+      <img src={search} alt="search" />
+      <input
+        type="text"
+        placeholder="Search from menu..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        className="search-input"
+      />
+    </div>
+  );
+};
 
 function Restaurant() {
   const location = useLocation();
   const { restaurantName } = location.state || {};
 
   return (
-    <div className='restaurant'>
+    <div className="restaurant">
       <Navbar />
       <RestaurantBanner />
-      <TitleBar title={`All Offers from ${restaurantName}`} />
+      <TitleBar title={`All Offers from ${restaurantName}`}>
+        <SearchBar />
+      </TitleBar>
       <Footer />
     </div>
-  )
+  );
 }
 
-export default Restaurant
+export default Restaurant;
